2eb1711e5fb102c6f39ca4ee0b6a6ed5f73c1d17
enunidad <133803895+enunidad@users.noreply.github.com>
2024-11-04 11:52:11 -0500

    upgrade comparatives
    explicitly defined comparatives for Card and Hand

--- Cards.py
+++ Cards.py
@@ -1,140 +1,151 @@
+class Rank:
+    def __init__(self, rank):
+        self.value = rank
+    @property
+    def index(self):
+        rank_map = {'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+        return int(rank_map.get(self.value, self.value))
+    def __repr__(self):
+        return f'Rank(rank = {self.value})'
+    def __str__(self):
+        return self.value
+    def __eq__(self, other):
+        return self.value == other.value
+    def __gt__(self, other):
+        return self.index > other.index
+    def __ge__(self, other):
+        return self > other or self == other
+    def __lt__(self, other):
+        return not self >= other
+    def __le__(self, other):
+        return self < other or self == other
+
+class Suit:
+    def __init__(self, shape):
+        self.value = shape
+    def __repr__(self):
+        return f'Suit(shape = {self.value})'
+    def __str__(self):
+        return self.value
+    def __eq__(self, other):
+        return self.value == other.value
+    
 class Card:
     def __init__(self, value, suit):
-        self.face = value
-        self.suit = suit
-    
-    @property
-    def value(self):
-        if self.face == 'A':
-            return [1, 14]
-        faces = {'J':11, 'Q':12, 'K':13}
-        if self.face in faces.keys():
-            return [faces[self.face]]
-        else:
-            return [int(self.face)]
-    
+        self.face = Rank(value)
+        self.suit = Suit(suit)
+    def __repr__(self):
+        return f'Card(value = {self.face.value}, suit = {self.suit.value})'
     def __str__(self):
-        return f'({self.face}, {self.suit})'
-    
+        return f'{str(self.face)}{str(self.suit)}'
+    def __eq__(self, other):
+        return self.face == other.face
     def __gt__(self, other):
-        return max(self.value) > max(other.value)
-    
+        return self.face > other.face
+    def __ge__(self, other):
+        return self > other or self == other
     def __lt__(self, other):
-        return max(self.value) < max(other.value)
+        return not self >= other
+    def __le__(self, other):
+        return self < other or self == other
 
 class Hand:
     def __init__(self, card1, card2, card3):
-        self.card1 = card1
-        self.card2 = card2
-        self.card3 = card3
-    
-    def __is_straight(self, values):
-        from itertools import product
-        for a, b, c in product(*values):
-            lst = sorted([a, b, c])
-            if lst[0] + 1 == lst[1] and lst[1] + 1 == lst[2]:
-                return True
-        return False
-    
-    def __is_flush(self, values):
-        if len(set(values)) == 1:
-            return True
-        return False
-    
-    def __is_trip(self, values):
-        from itertools import product
-        for a, b, c in product(*values):
-            if a==b and b ==c and a==c:
-                return True
-        return False
-    
-    def __is_pair(self, values):
-        from itertools import product
-        for a, b, c in product(*values):
-            if len(set([a, b, c])) == 2:
-                return True
-        return False
-    
-    def __str__(self):
-        return f'{str(self.card1)},{str(self.card2)},{str(self.card3)}'
-    
-    def get_rank(self):
-        hands = ['high card', 'pair', 'flush', 'straight', 'three of a kind', 'straight flush']
-        return hands.index(self.value)
-    
-    def __eq__(self, other):
-        my_rank = self.get_rank()
-        oth_rank = other.get_rank()
-        if my_rank == oth_rank:
-            my_values = self.order_cards()
-            oth_values = other.order_cards()
-            for m, o in zip(my_values, oth_values):
-                if m != o:
-                    return False
-            return True
-        else:
-            return False
-        
-    def count(self):
-        count = {}
-        for card in self.order_cards():
-            count[card] = count.get(card, 0) + 1
-        return count
-    
-    def __gt__(self, other):
-        my_rank = self.get_rank()
-        oth_rank = other.get_rank()
-        if my_rank == oth_rank:
-            if len(set(self.order_cards())) == 2:
-                my_count = sorted(list(self.count().items()), key = lambda x: -x[1])
-                oth_count = sorted(list(other.count().items()), key = lambda x: -x[1])
-                if my_count[0][0] > oth_count[0][0]:
-                    return True
-                elif my_count[0][0] == oth_count[0][0]:
-                    if my_count[1][0] > oth_count[1][0]:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                my_values = self.order_cards()
-                oth_values = other.order_cards()
-                for m, o in zip(my_values, oth_values):
-                    if m > o:
-                        return True
-                return False
-        elif my_rank > oth_rank:
-            return True
-        else:
-            return False
-    
-    def __lt__(self, other):
-        return not self.__eq__(other) and not self.__gt__(other)
-        
+        self.cards = [card1, card2, card3]
+
     @property
-    def highest_card(self):
-        faces = [self.card1.value, self.card2.value, self.card3.value]
-        return max([max(v) for v in faces])
-    
-    def order_cards(self):
-        faces = [self.card1.value, self.card2.value, self.card3.value]
-        return list(reversed(sorted([max(v) for v in faces])))
-    
+    def is_straight_flush(self):
+        straight, err1 = self.is_straight
+        flush, err2 = self.is_flush
+        if straight and flush:
+            return True, err1
+        else:
+            return False, None
+    @property
+    def is_straight(self):
+        valids = 'A23,234,345,456,567,678,789,89T,9TJ,TJQ,JQK,QKA'.split(',')
+        my_cards = set([card.face.value for card in self.cards])
+        for valid in valids:
+            ref_cards = set(valid)
+            if len(ref_cards ^ my_cards) == 0:
+                return True, sorted(self.cards, key = lambda x: -x.face)
+        return False, None
+    @property
+    def is_flush(self):
+        suits = [card.suit.value for card in self.cards]
+        if len(set(suits)) == 1:
+            return True, sorted(self.cards, key = lambda x: -x.face)
+        else:
+            return False, None
+    @property
+    def is_trip(self):
+        f = [card.face.value for card in self.cards]
+        if len(set(f)) == 1:
+            return True, sorted(self.cards, key = lambda x: -x.face)
+        else:
+            return False, None
+
+    @property
+    def is_pair(self):
+        f = [card.face.value for card in self.cards]
+        if len(set(f)) == 2:
+            if self.cards[0] == self.cards[1]:
+                to_return = self.cards
+            else:
+                to_return = self.cards[1:] + self.cards[:1]
+            return True, to_return
+        else:
+            return False, None
+
     @property
     def value(self):
-        suits = [self.card1.suit, self.card2.suit, self.card3.suit]
-        faces = [self.card1.value, self.card2.value, self.card3.value]
-        is_straight = self.__is_straight(faces)
-        is_flush = self.__is_flush(suits)
-        if is_straight and is_flush:
-            return 'straight flush'
-        if is_straight and not is_flush:
-            return 'straight'
-        if not is_straight and is_flush:
-            return 'flush'
-        if self.__is_trip(faces):
-            return 'three of a kind'
-        if self.__is_pair(faces):
-            return 'pair' 
-        return 'high card'
+        order = list(reversed(['pair', 'flush', 'straight', 'three of a kind', 'straight flush']))
+        for rank in order:
+            result, msg = eval(f'self.is_{rank}')
+            if result == True:
+                return rank, msg
+        return 'high card', sorted(self.cards, key = lambda x: -x.face)
+    @property
+    def index(self):
+        order = list(reversed(['pair', 'flush', 'straight', 'three of a kind', 'straight flush']))
+        for i, rank in enumerate(order):
+            result, msg = eval(f'self.is_{rank}')
+            if result == True:
+                return i, msg
+        return 0, sorted(self.cards, key = lambda x: -x.face)
+
+    def __eq__(self, other):
+        mine, m = self.index
+        your, y = other.index
+        if mine == your:
+            for s, o in zip(m, y):
+                if s != o:
+                    return False
+            return True
+        else:
+            return False
+                
+    def __gt__(self, other):
+        mine, m = self.index
+        your, y = other.index
+        if mine <= your:
+            if mine < your:
+                return True
+            else:
+                for s, o in zip(m, y):
+                    if s > o:
+                        return True
+                return False
+        else:
+            return False
+    def __ge__(self, other):
+        return self > other or self == other
+    def __lt__(self, other):
+        return not self >= other
+    def __le__(self, other):
+        retutn self < other or self == other
+        
+    def __repr__(self):
+        return 'Hand(' + ', '.join([f'card{i+1} = {card}'for i, card in enumerate(self.cards)]) + ')'
+    def __str__(self):
+        return f'{', '.join([str(card) for card in self.cards])}'
